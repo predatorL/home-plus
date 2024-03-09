abc165ce455ef526de7157cd367aa237546460cb
sishi.lhy <sishi.lhy@alibaba-inc.com>
2024-03-09 11:06:00 +0800

    feat: housekeep init

--- cms-server/types/generated/components.d.ts
+++ cms-server/types/generated/components.d.ts
@@ -1,5 +1,33 @@
 import type { Schema, Attribute } from '@strapi/strapi';
 
+export interface ListContent extends Schema.Component {
+  collectionName: 'components_list_contents';
+  info: {
+    displayName: 'content';
+  };
+  attributes: {
+    content: Attribute.String;
+    note: Attribute.String;
+  };
+}
+
+export interface ListImages extends Schema.Component {
+  collectionName: 'components_list_images';
+  info: {
+    displayName: 'images';
+    icon: 'bulletList';
+  };
+  attributes: {
+    content: Attribute.String;
+    image: Attribute.Media;
+  };
+}
+
 declare module '@strapi/types' {
-  export module Shared {}
+  export module Shared {
+    export interface Components {
+      'list.content': ListContent;
+      'list.images': ListImages;
+    }
+  }
 }
