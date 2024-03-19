60bcb55a44012179dd608e65e2429010aa5d7872
sishi.lhy <sishi.lhy@alibaba-inc.com>
2024-03-19 20:47:17 +0800

    feat(tita): 动画

--- app/tita/src/logics/game/init.js
+++ app/tita/src/logics/game/init.js
@@ -1,19 +1,48 @@
-import { Application, Text, Graphics, Sprite, Loader } from "pixi.js";
+import { Application, Assets, Text, Graphics, Texture, Sprite, Loader } from "pixi.js";
 
 export const GameInit = async () => {
-    // Create the application
+    console.log('gameInit');
     const app = new Application();
 
-    await app.init({ 
-        width: 800, 
+    await app.init({
+        width: 800,
         height: 600,
         transparent: true,
         backgroundColor: 0x6495ed
-     });
+    });
+
+    document.body.appendChild(app.canvas);
+    // const imgUrl = 'https://faas-image.amap.com/shaolin/images/G20/CP_LOGO/cp_flash@3x.png?t=1703488480000';
+    const imgUrl = '/guaiwu.webp';
+    // 加载图片，并在加载完毕后执行`onImageLoaded`函数
+    // Loader.shared.add('https://i.imgur.com/IaUrttj.png').load(onImageLoaded);
+    await Assets.load(imgUrl);
+    
+    let sprite = Sprite.from(imgUrl);
+    app.stage.addChild(sprite);
+
+    // Add a ticker callback to move the sprite back and forth
+    let elapsed = 0.0;
+    app.ticker.add((ticker) => {
+        elapsed += ticker.deltaTime;
+        sprite.y = 100.0 + Math.cos(elapsed/50.0) * 100.0;
+    });
+}
+
+export const GameInit2 = async () => {
+    // Create the application
+    const app = new Application();
+
+    await app.init({
+        width: 800,
+        height: 600,
+        transparent: true,
+        backgroundColor: 0x6495ed
+    });
 
     // Add the view to the DOM
     document.body.appendChild(app.canvas);
-    
+
     // 创建文本成员
     const slogan = new Text({
         text: 'Hello PixiJS!',
@@ -25,7 +54,7 @@ export const GameInit = async () => {
     slogan.position.set(20, 20);
     // 创建图形成员
     const sloganBg = new Graphics()
-    .rect(0, 0, slogan.width + 20, slogan.height + 20, 10);
+        .rect(0, 0, slogan.width + 20, slogan.height + 20, 10);
     sloganBg.fill({
         color: 'red',
         alpha: 0.5
@@ -35,7 +64,7 @@ export const GameInit = async () => {
     app.stage.addChild(slogan, sloganBg);
     setInterval(() => {
         slogan.text = Math.random().toString(36).substring(2);
-        sloganBg.position.set(Math.random() * 10, Math.random() *10);
+        sloganBg.position.set(Math.random() * 10, Math.random() * 10);
 
     }, 2000)
 }
