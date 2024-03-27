59a94881eede211a349a9584e2d84cf775505a84
sishi.lhy <sishi.lhy@alibaba-inc.com>
2024-03-27 10:15:48 +0800

    feat: 动画调试

--- app/tita/src/logics/GameInit.js
+++ app/tita/src/logics/GameInit.js
@@ -6,6 +6,12 @@ const initConfig = {
     backgroundColor: 0x6495ed
 }
 
+const imgUrl = '/guaiwu.webp';
+const iconUrl = '/icon1.jpg';
+
+
+const array = [1, 2, 3, 4, 5];
+
 export const GameInit = async () => {
     console.log('gameInit');
     const app = new Application();
@@ -13,16 +19,34 @@ export const GameInit = async () => {
     await app.init(initConfig);
 
     // 资源加载
-    const imgUrl = '/guaiwu.webp';
     await Assets.load(imgUrl);
-    let sprite = Sprite.from(imgUrl);
-    app.stage.addChild(sprite);
+    await Assets.load(iconUrl);
+    const spriteArray = array.map(item => {
+        const _sprite = Sprite.from(imgUrl);
+        _sprite.width  = 50;
+        _sprite.height = 50;
+        return _sprite;
+    });
+    spriteArray.forEach((item, i) => {
+        item.x += i * 100;
+        app.stage.addChild(item);
+    });
+    // app.stage.addChild(spriteArray);
 
     // Add a ticker callback to move the sprite back and forth
     let elapsed = 0.0;
     app.ticker.add((ticker) => {
         elapsed += ticker.deltaTime;
-        sprite.y = 100.0 + Math.cos(elapsed/50.0) * 100.0;
+        console.log(spriteArray[0].y)
+        spriteArray.forEach((item, i) => {
+            if (item.y > 600) {
+                item.y = 0;
+            } else {
+                // item.y += item.y + 0.005 * (i + 1);
+                // item.y += item.y + 1;
+            }
+        });
+        // sprite.y = 100.0 + Math.cos(elapsed/50.0) * 100.0;
     });
 
 
